c852f89d69a149bc40068ed72e740ade6aef5053
Anderson Spolavori <aspolavori@yahoo.com.br>
2021-02-24 22:20:55 +0000

    .

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import {Container} from './styles';
+//import logoImg from '../../assets/logo.png';
+
+const SignUp : React.FC = () => {
+  return (
+    <Container >
+     
+    </Container>
+  );
+}
+
+export default SignUp;
